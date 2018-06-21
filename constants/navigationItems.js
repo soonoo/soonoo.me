a9ee561d58465e72b51932a9494e0866f837a811
soonoo <qpseh2m7@gmail.com>
2018-06-21 22:27:44 +0900

    add constants directory

---
+++ constants/navigationItems.js
@@ -0,0 +1,17 @@
+const navigationItems = [
+	{
+		title: 'about',
+		link: '/about',
+	},
+	{
+		title: 'blah blah',
+		link: '/blah',
+	},
+	{
+		title: 'projects',
+		link: '/projects',
+	},
+];
+
+export default navigationItems;
+
